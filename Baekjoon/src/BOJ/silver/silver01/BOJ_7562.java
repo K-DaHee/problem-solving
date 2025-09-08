0d1a723fd67ca6f570cbccd63caa7958ae9c5cdd
김다희 <dhk086512@gmail.com>
2025-09-08 14:39:45 +0900

    [SEP/Baekjoon] BOJ 7562

---
+++ Baekjoon/src/BOJ/silver/silver01/BOJ_7562.java
@@ -0,0 +1,16 @@
+package BOJ.silver.silver01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_7562 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+	}
+
+}
