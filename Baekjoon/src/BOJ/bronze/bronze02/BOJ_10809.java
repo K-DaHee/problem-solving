b1d9a515de9382ff4aaaed2e44373039d26a530d
김다희 <dhk086512@gmail.com>
2025-10-20 10:42:22 +0900

    [OCT/BOJ] 10809 알파벳 찾기

---
+++ Baekjoon/src/BOJ/bronze/bronze02/BOJ_10809.java
@@ -0,0 +1,23 @@
+package BOJ.bronze.bronze02;
+
+import java.util.Scanner;
+
+public class BOJ_10809 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] position = new int[26];
+		for (int i = 0; i < position.length; i++) {
+			position[i] = -1;
+		}
+
+		char[] arr = sc.next().toCharArray();		
+		for (int i = 0; i < arr.length; i++) {
+			if (position[arr[i] - 'a'] == -1)
+				position[arr[i] - 'a'] = i;
+		}
+		
+		for (int i : position) {
+			System.out.print(i + " ");
+		}
+	}
+}
