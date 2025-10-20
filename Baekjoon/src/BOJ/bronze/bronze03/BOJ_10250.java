2dfbde9e7b6ab7d2bbc098c3ee94519ffc0c0c56
김다희 <dhk086512@gmail.com>
2025-10-20 10:58:06 +0900

    [OCT/BOJ] 10250 ACM 호텔

---
+++ Baekjoon/src/BOJ/bronze/bronze03/BOJ_10250.java
@@ -0,0 +1,26 @@
+package BOJ.bronze.bronze03;
+
+import java.util.Scanner;
+
+public class BOJ_10250 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int tc = 0; tc < t; tc++) {
+			int h = sc.nextInt();
+			int w = sc.nextInt();
+			int n = sc.nextInt();
+			
+			int count = 1;
+			while(n > h) {
+				count++;
+				n -= h;
+			}
+			String result = Integer.toString(count);
+			if(count < 10) {
+				result = "0" + Integer.toString(count);
+			}
+			System.out.println(n + result);
+		}
+	}
+}
