2510b868b4221c976cb858c3fb5db000a9af54ff
김다희 <dhk086512@gmail.com>
2026-03-01 19:11:48 +0900

    [SEP/BOJ] 13549 숨바꼭질 3 (#40)

---
+++ Baekjoon/src/BOJ/gold/gold05/BOJ_13549.java
@@ -0,0 +1,58 @@
+package BOJ.gold.gold05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_13549 {
+	static int min, minCount;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		minCount = 0;
+		bfs(n, k);
+		System.out.print(min);
+	}
+
+	public static void bfs(int n, int k) {
+		int start = n;
+
+		Queue<Integer> queue = new LinkedList<>();
+		int[] time = new int[100001];
+
+		// 시작점 큐에 추가하고 방문 처리
+		queue.add(start);
+		time[start] = 1;
+
+		// 큐가 빌 때까지 반복
+		while (!queue.isEmpty()) {
+			int current = queue.poll();
+
+			if (current == k)
+				min = time[current] - 1;
+			
+			// 다음 이동 위치 탐색
+			int[] nextPositions = {current * 2,  current - 1, current + 1};
+
+			for (int next : nextPositions) {
+				if (next < 0 || next >= 100001)
+					continue;
+				if (time[next] == 0 || time[next] == time[current] + 1) {
+					queue.add(next);
+					if (next == current * 2) {
+						time[next] = time[current];
+						continue;
+					}
+					time[next] = time[current] + 1;
+				}
+			}
+		}
+	}
+}
