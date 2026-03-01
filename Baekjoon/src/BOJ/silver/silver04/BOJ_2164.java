885d2e1728a2347ded25a7b69a082473b514ef52
김다희 <dhk086512@gmail.com>
2026-03-01 18:57:51 +0900

    [FEB/BOJ] 2164 카드2 (#34)

---
+++ Baekjoon/src/BOJ/silver/silver04/BOJ_2164.java
@@ -0,0 +1,25 @@
+package BOJ.silver.silver04;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_2164 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+        while (q.size() != 1) {
+            q.poll();
+            int x = q.poll();
+            q.offer(x);
+        }
+        System.out.println(q.poll());
+    }
+
+}
