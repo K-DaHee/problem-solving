d9c89b2d7108fb10b3367601b72ea2fa459b2741
김다희 <dhk086512@gmail.com>
2026-03-01 18:58:29 +0900

    [MAR/BOJ] 2231 분해합 (#37)
    * [MAR/BOJ] 2231 분해합
    
    * [MAR/BOJ] 2231 분해합

---
+++ Baekjoon/src/BOJ/bronze/bronze02/BOJ_2231_2.java
@@ -0,0 +1,36 @@
+package BOJ.bronze.bronze02;
+
+import java.util.Scanner;
+
+public class BOJ_2231_2 {
+    static int digitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int d = String.valueOf(N).length();
+
+        int start = N - 9 * d;
+        if (start < 1) start = 1;
+
+        int answer = 0;
+
+        for (int M = start; M < N; M++) {
+            int sum = M + digitSum(M);
+            if (sum == N) {
+                answer = M;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
