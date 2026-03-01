ff4fa1bfed04687a71193c31c493ec0486328b9b
김다희 <dhk086512@gmail.com>
2026-03-01 17:16:38 +0900

    [MAR/BOJ] 2231 분해합

---
+++ Baekjoon/src/BOJ/bronze/bronze02/BOJ_2231.java
@@ -0,0 +1,31 @@
+package BOJ.bronze.bronze02;
+
+import java.util.*;
+
+public class BOJ_2231 {
+
+    static int digitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int answer = 0;
+
+        for (int M = 1; M < N; M++) {
+            if (M + digitSum(M) == N) {
+                answer = M;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
