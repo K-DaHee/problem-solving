a07eecab6791f4ebdd3cc3907fcd0437977d042a
dahee <dhk086512@gmail.com>
2026-07-21 01:07:28 +0900

    [APR/BOJ] 2292 벌집 (#49)

---
+++ Baekjoon/src/BOJ/bronze/bronze02/BOJ_2292.java
@@ -0,0 +1,19 @@
+package BOJ.bronze.bronze02;
+
+import java.util.Scanner;
+
+public class BOJ_2292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int count = 1;
+
+        while (N > 1) {
+            N -= (6 * count);
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
