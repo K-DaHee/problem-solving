38fc7e36dcacc85560be69f3bb5bf8c3ceb4e7a8
dahee <dhk086512@gmail.com>
2026-07-21 01:07:22 +0900

    [APR/BOJ] 1259 팰린드롬수 (#50)
    * [APR/BOJ] 1259 팰린드롬수
    
    * [APR/BOJ] 1259 팰린드롬수
    
    * [APR/BOJ] 1259 팰린드롬수

---
+++ Baekjoon/src/BOJ/bronze/bronze01/BOJ_1259.java
@@ -0,0 +1,31 @@
+package BOJ.bronze.bronze01;
+
+import java.util.Scanner;
+
+public class BOJ_1259 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            String s = sc.next();
+            if(s.equals("0")) return;
+
+            char[] arr = s.toCharArray();
+
+            boolean isP = false;
+            for(int i = 0; i < arr.length; i++){
+                int start = i;
+                int end = arr.length - i - 1;
+
+                if(arr[start] == arr[end]) isP = true;
+                else {
+                    isP = false;
+                    break;
+                }
+            }
+
+            if(isP) System.out.println("yes");
+            else System.out.println("no");
+        }
+    }
+}
---
+++ Baekjoon/src/BOJ/bronze/bronze01/BOJ_1259_2.java
@@ -0,0 +1,30 @@
+package BOJ.bronze.bronze01;
+
+import java.util.Scanner;
+
+public class BOJ_1259_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+            String s = sc.next();
+            if(s.equals("0")) return;
+
+            boolean isP = true;
+            int len = s.length();
+
+            for(int i = 0; i < len/2; i++){
+                int start = i;
+                int end = len - i - 1;
+
+                if(s.charAt(start) != s.charAt(end)) {
+                    isP = false;
+                    break;
+                }
+            }
+
+            if(isP) System.out.println("yes");
+            else System.out.println("no");
+        }
+    }
+}
