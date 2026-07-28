913611a978ce715a9b771bffb0b97a4d47f5c0ea
dahee <dhk086512@gmail.com>
2026-07-28 16:14:36 +0900

    [JUL/PRO] 1844 게임 맵 최단거리 (#68)

--- Programmers/src/PRO/Lv2/PRO_1844.java
+++ Programmers/src/PRO/Lv2/PRO_1844.java
@@ -9,9 +9,8 @@ public class PRO_1844 {
 }
     
 class Solution_1844 {
-    // 상하좌우
-    int[] dr = {-1, 1, 0, 0};  // 행
-    int[] dc = {0, 0, -1, 1};  // 열
+    int[] dr = {0, 0, 1, -1};
+    int[] dc = {1, -1, 0, 0};
     boolean[][] visited;
     int min = Integer.MAX_VALUE;
     
@@ -21,35 +20,42 @@ class Solution_1844 {
         
         visited = new boolean[n][m];
         
-        int answer = bfs(n, m, maps);
-        if (answer == Integer.MAX_VALUE) answer = -1; 
-        return answer;
+        int min = bfs(n, m, maps);
+        
+        if (min == Integer.MAX_VALUE)
+            return -1;
+        
+        return min;
     }
     
-    public int bfs(int n, int m, int[][] maps){
+    int bfs (int n, int m, int[][] maps) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 1});  // 캐릭터 시작위치, 이동횟수
+        q.add(new int[]{0, 0, 1});
         visited[0][0] = true;
         
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curR = cur[0];
-            int curC = cur[1];
-            int dist = cur[2];
+            int[] current = q.poll();
+            int curR = current[0];
+            int curC = current[1];
+            int count = current[2];
             
             if (curR == n-1 && curC == m-1) {
-                min = Math.min(min, dist);
+                min = Math.min(min, count);
             }
             
-            for (int d = 0; d < 4; d++) {
-                int nR = curR + dr[d];
-                int nC = curC + dc[d];
-                if (nR < n && nR >= 0 && nC < m && nC >= 0 && maps[nR][nC] == 1 && !visited[nR][nC]) {
-                    q.add(new int[]{nR, nC, dist + 1});
-                    visited[nR][nC] = true;
-                }
+            for (int i = 0; i < 4; i++) {
+                int nR = curR + dr[i];
+                int nC = curC + dc[i];
+                
+                if (nR >= n || nR < 0 || nC >= m || nC < 0) continue;
+                if (visited[nR][nC]) continue;
+                if (maps[nR][nC] == 0) continue;
+                
+                q.add(new int[]{nR, nC, count + 1});
+                visited[nR][nC] = true;
             }
         }
+        
         return min;
     }
 }
