2abdcb09e6961c2d958fdaf8933f0d9bc12051aa
dahee <dhk086512@gmail.com>
2026-08-02 18:58:54 +0900

    [JUL/PRO] 389479 서버 증설 횟수 (#70)
    * [JUL/PRO] 389479 서버 증설 횟수
    
    * [JUL/PRO] 389479 서버 증설 횟수

---
+++ Programmers/src/PRO/Lv2/PRO_389479.java
@@ -0,0 +1,42 @@
+package PRO.Lv2;
+import java.util.*;
+
+public class PRO_389479 {
+  public static void main(String[] args) {
+      Solution_389479 s = new Solution_389479();
+      // 테스트케이스를 활용해 코드를 실행코드 작성하시오.
+  }
+}
+    
+class Solution_389479 {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+
+        Map<Integer, Integer> servers = new HashMap<>();
+        int serverId = 1;
+
+        for (int hour = 0; hour < 24; hour++) {
+
+            List<Integer> removeList = new ArrayList<>();
+
+            for (int id : servers.keySet()) {
+                if (servers.get(id) == hour) {
+                    removeList.add(id);
+                }
+            }
+
+            for (int id : removeList) {
+                servers.remove(id);
+            }
+
+            int required = players[hour] / m;
+
+            while (servers.size() < required) {
+                servers.put(serverId++, hour + k);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
